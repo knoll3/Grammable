5c92ec4fee2905d6b209fba210bf0b9f9741937d
Knoll <noll.kyle@gmail.com>
2018-07-01 14:28:17 -0500

    Edit and update gram

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -4,13 +4,6 @@ class GramsController < ApplicationController
   def index
   end
 
-  def show
-    @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render plain: 'Not Found', status: :not_found
-    end
-  end
-
   def new
     @gram = Gram.new
   end
@@ -24,9 +17,34 @@ class GramsController < ApplicationController
     end
   end
 
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    render_not_found if @gram.blank?
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    render_not_found if @gram.blank?
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id]) 
+    return render_not_found if @gram.blank?
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
     def gram_params
       params.require(:gram).permit(:message)
+    end
+
+    def render_not_found
+      render plain: 'Not Found', status: :not_found
     end
 end
